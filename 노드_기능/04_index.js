cdb02af46d270a66aa48300703e3e2023f132773
migyeong <rmfosem613@gmail.com>
2020-09-21 23:14:03 +0900

    Update 04_index.js

--- 노드_기능/04_index.js
+++ 노드_기능/04_index.js
@@ -1,4 +1,9 @@
+// var.js와 func.js를 모두 참조한다.
+// 모듈 하나가 여러 개의 모듈에 사용될 수 있다.
 const { odd, even } = require('./var');
+
+// 모듈로부터 값을 불러올 때 변수 이름을 다르게 지정할 수도 있다.
+// func.js의 checkOddOrEven이 checkNumber라는 이름으로 사용되고 있다.
 const checkNumber = require('./func');
 
 function checkStringOddOrEven(str) {
@@ -8,5 +13,8 @@ function checkStringOddOrEven(str) {
 	return even;
 }
 
+// func.js의 모듈인 checkOddOrEven함수에 11을 대입했을 때의 값이 반환
 console.log(checkNumber(11));
+
+// 함수에 'Hello'라는 문자열을 대입했을 대의 값이 반환
 console.log(checkStringOddOrEven('Hello'));
