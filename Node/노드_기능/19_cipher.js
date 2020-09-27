398499e4107ab3b56f12267a605429e1a0dc41fc
migyeong <rmfosem613@gmail.com>
2020-09-27 14:26:38 +0900

    Update 19_cipher.js

--- Node/노드_기능/19_cipher.js
+++ Node/노드_기능/19_cipher.js
@@ -3,41 +3,41 @@
 // 몇 가지 메서드는 익혀두면 실제 서비스에도 적용 할 수 있어 정말 유용하다.
 
 // 2. 양방향 암호화
-// 암호를 복호화하려면 암호화할 때 사용한 키와 같은 키를 사용해야 한다.
+// 암호화된 문장을 복호화할 수 있다.
+// 이때 암호화할 때 사용한 키와 같은 키가 사용된다.
 
 const crypto = require('crypto');
 
-// crypo.createCipher(알고리즘, 키) : 암호화 알고리즘은 aes-256-cbc를 사용했다.
-// 사용 가능한 알고리즘 목록은 crypto.getCiphers()를 하면 볼 수 있다.
-const cipher = crypto.createCipher('aes-256-cbc', '열쇠');
+const algorithm = 'aes-256-cbc';
+const key = 'abcedfghijklmnopqrstuvwxyz123456';
+const iv = '1234567890123456'; // iv는 16바이트여야 한다. 16개가 아니면 오류가 난다..
 
-// cipher.update(문자열, 인코딩, 출력 인코딩) : 암호화할 대상과 대상의 인코딩, 출력 결과물의 인코딩을 넣어준다.
+// crypto.createCipheriv(알고리즘, 키, iv) : 암호화할 때 사용
+// 사용 가능한 알고리즘 목록은 crypto.getCiphers()를 호출하면 볼 수 있다.
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+// cipher.update(문자열, 인코딩, 출력 인코딩) : 암호화할 대상과 대상의 인코딩, 출력 결과물의 인코딩을 넣는다.
 // 보통 문자열은 utf8 인코딩을,
-//  암호는 base64를 많이 사용한다.
+// 암호는 base64를 많이 사용한다.
 let result = cipher.update('암호화할 문장', 'utf8', 'base64');
 
-// crypto.final(출력 인코딩) : 출력 결과물의 인코딩을 넣어주면 암호화가 완료된다.
+// cipher.final(출력 인코딩) : 출력 결과물의 인코딩을 넣으면 암호화가 완료된다.
 result += cipher.final('base64');
 console.log('암호화: ', result);
 
-// crypto.createDecipher(알고리즘, 키) : 복호화할 때 사용한다.
-// 암호화할 때 사용했던 알고리즘과 키를 그대로 넣어주어야 한다.
-const decipher = crypto.createDecipher('aes-256-cbc', '열쇠');
+// crypto.createDecipheriv(알고리즘, 키, iv) : 복호화할 때 사용
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
 
-// decipher.update(문자열, 인코딩, 출력 인코딩) : 암호화된 문장, 그 문장의 인코딩, 복호화할 인코딩을 넣어준다.
-// createDecipher의 update()에서는 base64, utf8 순으로 넣으면 된다.
+// decipher.update(문자열, 인코딩, 출력 인코딩) : 암호화된 문장, 그 문장의 인코딩, 복호화할 인코딩을 넣는다.
 let result2 = decipher.update(result, 'base64', 'utf8');
 
-// decipher.final(출력 인코딩) : 복호화 결과물의 인코딩을 넣어준다.
-result2 +=decipher.final('utf8');
+// decipher.final(출력 인코딩) : 복호화할 결과물의 인코딩을 넣는다.
+result2 += decipher.final('utf8');
 console.log('복호화: ', result2);
 
 
-// 왜 error가 났는지 모르겠다.
-// [DEP0106] DeprecationWarning: crypto.createCipher is deprecated.
-// 안 졸릴 때 해결해야겠다.
 
-// 아... 사라진 기능이다.
+
 
 
 
