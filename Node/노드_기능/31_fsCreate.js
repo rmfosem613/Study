4e4e32276cccab793dcdda31e57180750fb55a5f
migyeong <rmfosem613@gmail.com>
2020-09-27 16:21:14 +0900

    Update 31_fsCreate.js
    프로미스 기반 fs모듈로 재설정
    constants 모듈을 설치해 바로 메서드를 사용할 수 있게됨

--- Node/노드_기능/31_fsCreate.js
+++ Node/노드_기능/31_fsCreate.js
@@ -4,7 +4,8 @@
 // 파일을 생성하고 삭제할 수도 있고, 폴더를 생성하고 삭제할 수도 있다.
 
 
-const fs = require('fs');
+const fs = require('fs').promises;
+const constants = require('fs').constants;
 
 // 모두 비동기 메서드이므로 한 메서드의 콜백에서 다른 메서드를 호출한다.
 
@@ -16,49 +17,39 @@ const fs = require('fs');
 // 를 체크하는 것이다.
 // 파일/폴더나 권한이 없다면 에러가 발생하는데,
 // 파일/폴더가 없을 때의 에러 코드는 ENOENT이다.
-fs.access('./folder', fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK, (err) => {
- 	if(err) {
- 		if(err.code === 'ENOENT') {
- 			console.log('폴더 없음');
+fs.access('./folder', constants.F_OK | constants.R_OK | constants.W_OK).then(() => {
+	return Promise.reject('이미 폴더 있음');
+}).catch((err) => {
+	if(err.code === 'ENOENT') {
+		console.log('폴더 없음');
 
- 			// fs.mkdir(경로, 콜백) : 폴더를 만드는 메서드
- 			// 이미 폴더가 있다면 에러가 발생하므로
- 			// 먼저 access() 메서드를 호출해서 확인하는 것이 중요하다.
- 			fs.mkdir('./folder', (err) => {
- 				if(err) {
- 					throw err;
- 				}
- 				console.log('폴더 만들기 성공');
+		// fs.mkdir(경로, 콜백) : 폴더를 만드는 메서드
+ 		// 이미 폴더가 있다면 에러가 발생하므로
+ 		// 먼저 access() 메서드를 호출해서 확인하는 것이 중요하다.
+		return fs.mkdir('./folder');
+	}
+	return Promise.reject(err);
+}).then(() => {
+ 	console.log('폴더 만들기 성공');
 
- 				//fs.open(경로, 옵션, 콜백) : 파일의 아이디(fs 변수)를 가져오는 메서드
- 				// 파일이 없다면 파일을 생성한 뒤 그 아이디를 가져온다.
- 				// 가져온 아이디를 사용해 fs.read()나 fs.write()로 읽거나 쓸 수 있다.
- 				// 두 번째 인자로 어떤 동작을 할 것인지 설정할 수 있다.
- 				// 쓰려면 w, 읽으려면 r, 기존 파일에 추가하려면 a이다.
- 				// w는 파일이 없을 때 새로 만들 수 있다.
- 				// r이면 에러가 뜬다.
- 				fs.open('./folder/file.js', 'w', (err,fd) => {
- 					if(err) {
- 						throw err;
- 					}
- 					console.log('빈 파일 만들기 성공', fd);
- 					
- 					// fs.rename(기존 경로, 새 경로, 콜백) : 파일의 이름을 바꾸는 메서드이다.
- 					// 기존 파일 위치와 새로운 파일 위치를 적어주면 된다.
- 					// 반드시 같은 폴더를 지정할 필요는 없으므로 잘라내기 같은 기능을 할 수도 있다.
- 					fs.rename('./folder/file.js', './folder/newfile.js', (err) =>{
- 						if(err) {
- 							throw err;
- 						}
- 						console.log('이름 바꾸기 성공');
- 					});
- 				});
- 			});
- 		} else {
- 			throw err;
- 		}
- 	} else {
- 		console.log('이미 폴더 있음');
- 	}
- });
+ 	//fs.open(경로, 옵션, 콜백) : 파일의 아이디(fs 변수)를 가져오는 메서드
+ 	// 파일이 없다면 파일을 생성한 뒤 그 아이디를 가져온다.
+ 	// 가져온 아이디를 사용해 fs.read()나 fs.write()로 읽거나 쓸 수 있다.
+ 	// 두 번째 인자로 어떤 동작을 할 것인지 설정할 수 있다.
+ 	// 쓰려면 w, 읽으려면 r, 기존 파일에 추가하려면 a이다.
+ 	// w는 파일이 없을 때 새로 만들 수 있다.
+ 	// r이면 에러가 뜬다.
+	return fs.open('./folder/file.js', 'w');
+}).then((fd) => {
+	console.log('빈 파일 만들기 성공', fd);
+
+	// fs.rename(기존 경로, 새 경로, 콜백) : 파일의 이름을 바꾸는 메서드이다.
+ 	// 기존 파일 위치와 새로운 파일 위치를 적어주면 된다.
+ 	// 반드시 같은 폴더를 지정할 필요는 없으므로 잘라내기 같은 기능을 할 수도 있다.
+	fs.rename('./folder/file.js', './folder/newfile.js');
+}).then(() => {
+	console.log('이름 바꾸기 성공');
+}).catch((err) => {
+	console.error(err);
+});
 
