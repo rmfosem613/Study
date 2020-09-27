8ea114bb59648b31e2a57ced70bde4a250d66a75
migyeong <rmfosem613@gmail.com>
2020-09-27 13:49:38 +0900

    Update 07_console.js

--- Node/노드_기능/07_console.js
+++ Node/노드_기능/07_console.js
@@ -39,6 +39,11 @@ console.log('평범한 로그입니다. 쉼표로 구분해 여러 값을 찍을
 console.log(string, number, boolean);
 console.error('에러 메시지는 console.error에 담아주세요.');
 
+
+// console.table(배열): 배열의 요소로 객체 리터럴을 넣으면,
+// 객체의 속성들이 테이블 형식으로 표현된다.
+console.table([{ name: '제로', birth: 1994}, { name: 'hero', birth: 1988}]);
+
 // console.dir(객체, 옵션) : 객체를 콘솔에 표시할 때 사용
 // 첫 번째 인자 = 표시할 객체,
 // 두 번째 인자 = 옵션 {colors:boolean, depth:기본 2}
