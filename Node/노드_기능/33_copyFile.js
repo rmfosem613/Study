1dcd41c1f3b24c46dd6c349b619d4b96ef398d40
migyeong <rmfosem613@gmail.com>
2020-09-27 16:25:16 +0900

    Update 33_copyFile.js
    프로미스 기반의 fs 모듈로 재설정

--- Node/노드_기능/33_copyFile.js
+++ Node/노드_기능/33_copyFile.js
@@ -1,16 +1,15 @@
 // 노드 8.5 버전에서는 파일 복사 방법도 새로 추가되었다.
 // 더이상 createReadStream과 createWriteStream을 pipe하지 않아도 된다.
 
-const fs = require('fs');
+const fs = require('fs'). promises;
 
 // 첫번째 인자로 복사할 파일을,
 // 두번째 인자로 복사된 경로를,
 // 세번째 인자로 복사 후 실행될 콜백 함수를 넣는다.
-fs.copyFile('readme4.txt', 'write4.txt', (error) => {
-	if(error) {
-		return console.log(error);
-	}
+fs.copyFile('readme4.txt', 'write4.txt').then(() => {
 	console.log('복사 완료');
+}).catch((error) => {
+	console.error(error);
 });
 
 
