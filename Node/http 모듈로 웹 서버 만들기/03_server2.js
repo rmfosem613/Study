fbe268fa5b43921e15d563f2b95fdbc87b37e997
migyeong <rmfosem613@gmail.com>
2020-09-27 17:03:22 +0900

    change order

--- Node/http 모듈로 웹 서버 만들기/03_server2.js
+++ Node/http 모듈로 웹 서버 만들기/03_server2.js
@@ -1,22 +1,29 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 
-http.createServer((req, res) => {
-
-	// 요청이 들어오면 먼저 fs 모듈로 HTML 파일을 읽는다.
-	// data 변수에 저장된 버퍼를 그래로 클라이언트에 보내주면 된다.
-	// 이전에는 문자열을 보냈지만 아래와 같이 버퍼를 보낼 수도 있다.
-	fs.readFile('./03_server2.html', (err, data) => {
-		if(err) {
-			throw err;
-		}
+http.createServer(async (req, res) => {
+	try{
+		const data = await fs.readFile('./03_server2.html');
+		res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
 		res.end(data);
-	});
+	} catch(err) {
+		console.error(err);
+		res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8'});
+		res.end(err.message);
+	}
 }).listen(8081, () => {
-	console.log('8081번 포트에서 서버 대기 중입니다!');
-});
+		console.log('8081번 포트에서 서버 대기 중입니다!');
+	});
 
-// HTML 파일을 읽어와 클라이언트로 전송하는 데 성공했다.
-// 하지만 현재 서버는 클라이언트가 누구인지 모른다.
-// 그냥 요청이 올 때 모두에게 같은 응답을 보내고 있다.
-// 다음에서 서버가 클라이언트가 누구인지 기억해서 클라이언트별로 다르게 응답하는 방법을 알아보자.
+// 요청이 들어오면 먼저 fs 모듈로 HTML 파일을 읽는다.
+// data 변수에 저장된 버퍼를 그대로 클라이언트에 보내면 된다.
+// 이전 예제에서는 문자열을 보냈지만 위와 같이 버퍼를 보낼 수도 있다.
+// 에러 메시지는 일반 문자열이므로 text/plain을 사용했다.
+
+// Note!!
+// HTTP 상태 코드
+//	- 2XX : 성골을 알리는 상태 코드. 대표적으로 200(성공), 201(작성됨)이 많이 사용됨.
+//	- 3XX : 리다이렉션(다른 페이지로 이동)을 알리는 상태 코드. 대표적으로 301(영구 이동), 302(임시이동)이 있음
+//	- 4XX : 요청 오류를 나타냄. 대표적으로 400(잘못된 요청), 401(권한 없음), 403(찾을 수 없음)이 많이 사용됨.
+//	- 5XX : 서버 오류를 나타냄. 요청은 제대로 왔지만 서버에 오류가 생겼을 때 발생한다.
+//          대표적으로 500(내부 서버 오류), 502(불량 게이트웨이), 503(서비스를 사용할 수 없음)이 자주 사용된다.
